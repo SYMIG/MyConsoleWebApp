5a0e9b3d0db754d02284e087ed0c1676fd22e049
SYMIG <90121399+SYMIG@users.noreply.github.com>
2021-12-19 22:26:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Samarth's";
+var last_name = "World";
+var full_name = first_name.concat(last_name);
+console.log(full_name)
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(add)
+var subtract = a - b ;
+console.log(subtract)
+var multiply = a * b ;
+console.log(multiply)
+var divide = a / b ;
+console.log(divide)       
